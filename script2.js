9094b5aa49097e5b97d94962da54a0c63690e3cf
IndicadoresDeVendas <140733197+IndicadoresDeVendas@users.noreply.github.com>
2023-07-31 08:11:51 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,187 @@
+function verificarLogin() {
+    var inputSenha = document.getElementById('inputSenha').value;
+    var AlexsandraMaria = "Alexsandra01";
+    var FernandaPessoa = "Fernanda02";
+    var AylaPatricia = "Ayla03";
+    var AlanaMilena = "Alana04";
+    var AnaBeatriz = "AnaB05";
+    var GabrielaRocha = "Gabriela06"; 
+    var CamilleEduarda = "Camille08";
+    var AlanaKerolayne = "AlanaK09";
+    var HortenciaMaria = "Hortencia10"; 
+    var LarissaCristina = "LarissaC11";
+    var DanielGomes = "Daniel12";
+    var AnndreynaHelloiza = "Anndreyna13";
+    var Annakaroliny = "Anna14";
+    var MariaGlaucione = "MariaG15";
+    var KarlaRonalda = "Karla16";
+    var CesarAugusto = "Cesar17"; 
+    var GabrielaAlmeida = "Gabriela18"; 
+    var ClaraSamara = "Clara19";
+    var EdigenilmaVieira = "Edigenilma20"; 
+    var LucivaniaXavier = "Lucivania21"; 
+    var ElizabethSilva = "Elizabeth22";
+    var Ilariavitoria = "Ilaria23";
+    var JadiaelFerreira = "Jadiel24";
+    var Annecristine = "Anne25";
+    var Priscilasilva = "Priscila26"; 
+    var MargarethNobre = "Margare27";
+    var LauraFernanda = "Laura28"; 
+    var FernandaCarolina = "Fernanda29";
+    var DamianaPasqualina = "Damiana30";
+    var OvaneOtavio = "Ovane31"; 
+    var RenataBispo = "Renata32"; 
+    var EwertonIgor = "Ewerton33";
+    var JessicaFranciele = "Jessica34";
+    var MariliaGabriele = "Marilia35";
+    var IvanaLopes = "Ivana36";
+    var ShayneTenorio = "Shayne37"; 
+    var LaineLilian = "Laine38";
+    var MarianaFernanda = "MariaF39";
+    var JoyceArisa = "Joyce40";
+    var LeticiaNeves = "Leticia41"; 
+    var MarcellyEduarda = "Marcelly42"; 
+    var DeboraFelipe = "Debora43"; 
+    var LarissaBarbosa = "LarissaB44";
+    var VanessaCristina = "Vanessa45"; 
+    var PauloAsaffe = "Paulo46"; 
+    var MayraNayara = "Mayra47";
+    var Renanwillamy = "Renan48";
+    var JulianaGonzaga = "Juliana49";
+    var NadiniCoimbra = "Nadini50";
+    var YasminMelo = "Yasmin51";
+    var MariaJulia = "Maria52"; 
+    var RafaelySouto = "Rafaely53";
+    var RoseliaPereira = "Roselia54";
+    var MariaCarla = "Maria55";
+    var SaraRaquel = "Sara56";
+    var WellingtonRibeiro = "Wellington57"; 
+    var GabrielleGomes = "Gabrielle58"; 
+    var ConceicaoMaria = "Conceicao59"; 
+    var anaKely = "AnaK60";
+
+  
+    if (inputSenha === AlexsandraMaria) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/15ge_gE5k-W7PwDjJwbLEMYK7hDSIqfDg--kJtOnGck4/edit?usp=sharing';
+    } else if (inputSenha === FernandaPessoa) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1RanTkJ608WSEpwMtOeShRPIgfFFcCg3wacy6lWM82nY/edit?usp=sharing';
+    } else if (inputSenha === AylaPatricia) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1WGRnoUxmtYdOTsZQc4FKp6ODjf4-9J3hRA2ugFL3Dso/edit?usp=sharing';
+    } else if (inputSenha === AlanaMilena) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1sBeq4EqgOGOOw7AShhxpFavdwYngp9GaHFubkwBfjbI/edit?usp=sharing';
+    } else if (inputSenha === AnaBeatriz) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/10YTo9HY1u_yExGdkCXAAZTfEBtUc2VP400i1AT1YrUs/edit?usp=sharing';
+    } else if (inputSenha === GabrielaRocha) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1JZDZM20NAamKxuWS_CWHI88qWZ6ShZiak69kLYxoblQ/edit?usp=sharing';
+    } else if (inputSenha === CamilleEduarda) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1mPMexFZFTbwgnrZvMPhw0OXy1GgIRCK3Tl-DcC1nD80/edit?usp=sharing';
+    } else if (inputSenha === AlanaKerolayne) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/12bBM-I8DZlmX2TUmVJ_1mvjb8t3izXdWKJLAy1dTmDQ/edit?usp=sharing';
+    } else if (inputSenha === HortenciaMaria) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1dPGSdi9LtA7kyFGfcyFGSVZxbbU6KiM6k21fAu0yG0U/edit?usp=sharing';
+    } else if (inputSenha === LarissaCristina) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/19A988jCdEHb_97y35SMwGCN0GZL17jNrBOBrkwQSS70/edit?usp=sharing';
+    } else if (inputSenha === DanielGomes) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1rlViLxoy_z05NTnxacccktA-tvTg7fg2EuuTi1XpTOA/edit?usp=sharing';
+    } else if (inputSenha === AnndreynaHelloiza) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1IKmN8qwFffhez77tTCQXl_4HYTgMBw8uuVa393XRshc/edit?usp=sharing';
+    } else if (inputSenha === Annakaroliny) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1cJpPVwGOjKHBky2yD5JZmfG6GqvxWnbSOC9E0-BYODs/edit?usp=sharing';
+    } else if (inputSenha === MariaGlaucione) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1FXGspx_7B6PrEHNPaMhs7ZVcMexYcRmh-I15aB0tuCo/edit?usp=sharing';
+    } else if (inputSenha === KarlaRonalda) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1p5ZcJhRJtbEn2ltoMzWSWpSXULFgzejg7F8sy8IR8Zw/edit?usp=sharing';
+    } else if (inputSenha === CesarAugusto) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1fX_KazzolNRcrRFpCCTS9QZ97ID1g9qEyPJ5SbFRbSw/edit?usp=sharing';
+    } else if (inputSenha === GabrielaAlmeida) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1_szDyU0dqQS6NZsfOsi8ROAjU0HKnE193y-kmpqAZxs/edit?usp=sharing';
+    } else if (inputSenha === ClaraSamara) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1S4jih07-XZx0LOAuXOoQFfGtjA-puKBU-6n4fLczZF4/edit?usp=sharing';
+    } else if (inputSenha === EdigenilmaVieira) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1cZJdSZbSwwRXmr8rScswfZVtDBKI3sHYOA5g7nI-rzU/edit?usp=sharing';
+    } else if (inputSenha === LucivaniaXavier) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1eoH9K1t7Ek1j7GmVXAAZlBbTCH2MCmls6rGASvsNUj0/edit?usp=sharing';
+    } else if (inputSenha === ElizabethSilva) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1a1_BGQzrxO7n6Whazs4tfWMInYVde5YkMnYDjzPuBoU/edit?usp=sharing';
+    } else if (inputSenha === Ilariavitoria) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1qobS0D7v_1majjCU1SA8uAIdSueDcfIo6zL3KeC9T6g/edit?usp=sharing';
+    } else if (inputSenha === JadiaelFerreira) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1x1-YUl9BQ9yaUOSauHIdedYOit-_hoFZ5ydVmC4qnF4/edit?usp=sharing';
+    } else if (inputSenha === Annecristine) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/13aqrK12hL1pGjuLLWiUMllkasfrIL8gl9JgB-IuYqmk/edit?usp=sharing';
+    } else if (inputSenha === Priscilasilva) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1h_RmMhQUDGDPTOMdffdKmuDvi5Z8y8mE-06TKN7P6Lg/edit?usp=sharing';
+    } else if (inputSenha === MargarethNobre) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1ItTS0DCUJXQCAu3E84WfLPtBeRIHJ5q1HziDncbeHgE/edit?usp=sharing';
+    } else if (inputSenha === LauraFernanda) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1MiwOxCYLjCQUoVces7CKkGXrX-6ixFBWKZ2Qf3bJO7k/edit?usp=sharing';
+    } else if (inputSenha === FernandaCarolina) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1eJwihcVo0ExxiX77tCRCGKPa7PyHJ2tfVHUKbALN5K0/edit?usp=sharing';
+    } else if (inputSenha === DamianaPasqualina) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1_4ngurNmxEjBavFjRpYivAZ6LIkUWVAqzN5Ymb6lwO8/edit?usp=sharing';
+    } else if (inputSenha === OvaneOtavio) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1vp2N66JqWbvLlO6nFwMTEdIMpMbYgHEymUjSdcGDGIU/edit?usp=sharing';
+    } else if (inputSenha === RenataBispo) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/18PVslXzg7VmenYqmyvkU6enydgH4KIoWzn7diRTybGA/edit?usp=sharing';
+    } else if (inputSenha === EwertonIgor) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/13a5MWNZ_4K0Xid57UWwYcvCC_MDfrt2mGAcFY7e_E-M/edit?usp=sharing';
+    } else if (inputSenha === JessicaFranciele) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/13a5MWNZ_4K0Xid57UWwYcvCC_MDfrt2mGAcFY7e_E-M/edit?usp=sharing';
+    } else if (inputSenha === MariliaGabriele) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1h_uXmc2f2QvYemjJfi5NtC2c_PFwDLPK7017M5-HiTw/edit?usp=sharing';
+    } else if (inputSenha === IvanaLopes) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1UlGwGvNy7tZ8utDB78PNT_4noXK6uZyw0ELAp42-81c/edit?usp=sharing';
+    } else if (inputSenha === ShayneTenorio) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/18DrgFO2WDjNvWpgRUD3-jkIHblyvH_GXgW5ngzmhJw4/edit?usp=sharing';
+    } else if (inputSenha === LaineLilian) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1PbZm6VwBRgthhIAlVCIb49CHj6pO7Sr2HflbGquI3yU/edit?usp=sharing';
+    } else if (inputSenha === MarianaFernanda) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1b3qICFlRz9iHRA02fYrOWq89W1D0gVVUcpBIxfRdWB0/edit?usp=sharing';
+    } else if (inputSenha === JoyceArisa) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1mG3X9H_ldNpbMQPESY_-Uy3PRrKfDq4-Pr1q6vUFc1U/edit?usp=sharing';
+    } else if (inputSenha === LeticiaNeves) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1N_VRhaDY2zZdQryzI_JEBUIbLNNGJsy-rn9nTWED_Xs/edit?usp=sharing';
+    } else if (inputSenha === MarcellyEduarda) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1Llc5UM7MuNu_TofD5GrTS1z047tQdrXs-ciLto2XJvw/edit?usp=sharing';
+    } else if (inputSenha === DeboraFelipe) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1a9R5nGRNEJY4UFyT50TF6JdSyvOJUln9tFMcWUgjJ9c/edit?usp=sharing';
+    } else if (inputSenha === LarissaBarbosa) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1Rmy5KANB07hfS7luP79f2oEeirasKn4ymQ7XWUP5zdE/edit?usp=sharing';
+    } else if (inputSenha === VanessaCristina) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1OPIZwrh-i-UpSpRx0k-RlJjtX7r9n7Qz93vJ1H-PAcU/edit?usp=sharing';
+    } else if (inputSenha === PauloAsaffe) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1_YjeHd2RfYkOPB8kPxiZcQanJQDZxvskJP5NTlPvr-o/edit?usp=sharing';
+    } else if (inputSenha === MayraNayara) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1-V6EHldKYNUWyXVsiqHNMSmaRCFBG4q6QVS_JLZYJjY/edit?usp=sharing';
+    } else if (inputSenha === Renanwillamy) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1aYiSevkjuEsF2FxZIePRrjNVMi-PKqDqZzk6NH4tnAA/edit?usp=sharing';
+    } else if (inputSenha === JulianaGonzaga) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1WfoI8Q4h6ZiP09gTPV66l6N8X4aS66_VDlf2soLV-i4/edit?usp=sharing';
+    } else if (inputSenha === NadiniCoimbra) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1gidufy6XesNfHHMqite1NaUd5Cy4v0yQ3oktEKu0MfY/edit?usp=sharing';
+    } else if (inputSenha === YasminMelo) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1deqIAe8B__nXKRFAonKQEPXKSaevenq06ex2XAVVSkk/edit?usp=sharing';
+    } else if (inputSenha === MariaJulia) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1jabQ-4huDvmdRKqZ9sr3irAY8vHp6SmkgK-SmGxVwuY/edit?usp=sharing';
+    } else if (inputSenha === RafaelySouto) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1Ata1YC7D5NN9oQfZOQbi3f3Kc2iXn4M5k3ouVGSgPB8/edit?usp=sharing';
+    } else if (inputSenha === RoseliaPereira) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1sghdVKcAUcslEt9Fva767IgMVVyBeplJ3Z-PrJppi3M/edit?usp=sharing';
+    } else if (inputSenha === MariaCarla) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1fKNv-tjC6nRQ1dC7Gw6Ez1Txvs2b-ZvTwG3n-6rr2aU/edit?usp=sharing';
+    } else if (inputSenha === SaraRaquel) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/109hs7PkAxK2KM2_7u0LYtoO0NwwV9MUl8HxpEemd10c/edit?usp=sharing';
+    } else if (inputSenha === WellingtonRibeiro) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1wSG0jO0-w6a5DqxRB4daakbA8BSL6KFxjCcd-af6ucE/edit?usp=sharing';
+    } else if (inputSenha === GabrielleGomes) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1rApx1Cna5GPZaySbFQLLL8GVAER4Ef8ZhB0ntrNTvSM/edit?usp=sharing';
+    } else if (inputSenha === ConceicaoMaria) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1JHE4yYQWdLciYw7p4Y2sBfSgTmYNNY2tTWQ-pN5Dpag/edit?usp=sharing';
+    } else if (inputSenha === anaKely) {
+      window.location.href = 'https://docs.google.com/spreadsheets/d/1dgUMckC6siL2CPN1sTGwVvVJ8wZGsXp54u3KERO09-w/edit?usp=sharing';
+    }
+    
+    else {
+      alert('Senha incorreta. Tente novamente.');
+    }
+  }  
